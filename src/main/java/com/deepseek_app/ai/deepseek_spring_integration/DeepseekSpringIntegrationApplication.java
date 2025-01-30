13c77ba035a3343bd4fac89a526b967296166dce
saurav_ihb <sauravk@ihbl.in>
2025-01-30 20:54:37 +0530

    author

--- src/main/java/com/deepseek_app/ai/deepseek_spring_integration/DeepseekSpringIntegrationApplication.java
+++ src/main/java/com/deepseek_app/ai/deepseek_spring_integration/DeepseekSpringIntegrationApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * @author Sauravsku
+ */
 @SpringBootApplication
 public class DeepseekSpringIntegrationApplication {
 
@@ -17,8 +20,9 @@ public class DeepseekSpringIntegrationApplication {
 	public CommandLineRunner runner(ChatClient.Builder builder) {
 		return args -> {
 			ChatClient chatClient = builder.build();
-			String response = chatClient.prompt("Tell a brief history of Java programming language.").call().content();
+			String response = chatClient.prompt("Tell me how to call").call().content();
 			System.out.println(response);
 		};
 	}
+
 }
